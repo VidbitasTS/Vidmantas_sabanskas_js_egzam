f9dbd5e5a3b39d8688d760faa8e4c0e44e103826
vidbitas <vidbitas@gmail.com>
2022-08-17 15:12:15 +0300

    Task1 done

--- Task 1/script.js
+++ Task 1/script.js
@@ -9,17 +9,38 @@ Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
 
+// Taikomes
 const formEl = document.forms[0];
 const outEl = document.querySelector('#output');
 const kgEl = document.querySelector('#search');
+const alertEl = document.querySelector('#alert');
+
+// addEventListner
 
 formEl.addEventListener('submit', (e) => {
     e.preventDefault();
+    outEl.style.display = 'none';
+    if (!kgEl.value) {
+        alertText(alertEl);
+        return;
+    }
+    outEl.style.display = 'inline-block';
+
     const lb = kgEl.value * 2.2046;
     const gr = kgEl.value / 0.001;
     const oz = kgEl.value * 35.274;
-
-    console.log(lb, gr, oz);
-
-    outEl.innerHTML = `<p><span>Svarai:</span> ${lb}</p><p><span>Gramai:</span> ${gr}</p><span>Uncijos:</span> ${oz}</p>`;
+    createOutputHtml(lb, gr, oz);
 });
+
+// Funkcijos
+
+function createOutputHtml(lb, gr, oz) {
+    outEl.innerHTML = `<p><span>Svarai:</span> ${lb}</p><p><span>Gramai:</span> ${gr}</p><span>Uncijos:</span> ${oz}</p>`;
+}
+
+function alertText(element) {
+    element.textContent = 'Neivedėt duomenų !!!';
+    setTimeout(() => {
+        element.textContent = '';
+    }, 2000);
+}
