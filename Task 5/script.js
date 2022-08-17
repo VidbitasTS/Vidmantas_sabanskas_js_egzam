a2e7d591843b9161d2024db53ffb7be1a2b27d55
vidbitas <vidbitas@gmail.com>
2022-08-17 23:12:40 +0300

    Task5 done

--- Task 5/script.js
+++ Task 5/script.js
@@ -9,13 +9,74 @@ atveju grąžins masyvą su "users", kurie yra pilnamečiai.
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20, hasDog: true },
-  { id: '2', name: 'Ann Smith', age: 24, hasDog: false },
-  { id: '3', name: 'Tom Jones', age: 31, hasDog: true },
-  { id: '4', name: 'Rose Peterson', age: 17, hasDog: false },
-  { id: '5', name: 'Alex John', age: 25, hasDog: true },
-  { id: '6', name: 'Ronald Jones', age: 63, hasDog: true },
-  { id: '7', name: 'Elton Smith', age: 16, hasDog: true },
-  { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
-  { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
+    { id: '1', name: 'John Smith', age: 20, hasDog: true },
+    { id: '2', name: 'Ann Smith', age: 24, hasDog: false },
+    { id: '3', name: 'Tom Jones', age: 31, hasDog: true },
+    { id: '4', name: 'Rose Peterson', age: 17, hasDog: false },
+    { id: '5', name: 'Alex John', age: 25, hasDog: true },
+    { id: '6', name: 'Ronald Jones', age: 63, hasDog: true },
+    { id: '7', name: 'Elton Smith', age: 16, hasDog: true },
+    { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
+    { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+function filterDogOwers(arr) {
+    return arr.filter((el) => el.hasDog === true);
+}
+const rez1 = filterDogOwers(users);
+console.log('Turi suni === ', rez1);
+
+function filterDogOwers1(arr) {
+    let newArr = [];
+    arr.forEach((el) => {
+        if (el.hasDog === true) newArr.push({ name: el.name, hasDog: el.hasDog });
+    });
+    return newArr;
+}
+const rez11 = filterDogOwers1(users);
+console.log('Turi suni 2 === ', rez11);
+
+function filterDogOwers2(arr) {
+    let newArr = [];
+    arr.filter(function(el) {
+        if (el.hasDog === true) {
+            newArr.push({ name: el.name, hasDog: el.hasDog });
+        }
+    });
+    return newArr;
+}
+
+const rez111 = filterDogOwers2(users);
+console.log('Turi suni 3 === ', rez111);
+
+// ======================
+
+function filterAdults(arr) {
+    return arr.filter((el) => el.age >= 18);
+}
+
+const rez2 = filterAdults(users);
+console.log('Pilnametis === ', rez2);
+
+function filterAdults1(arr) {
+    let newArr = [];
+    arr.forEach((el) => {
+        if (el.age >= 18) newArr.push({ name: el.name, age: el.age });
+    });
+    return newArr;
+}
+const rez22 = filterAdults1(users);
+console.log('Pilnametis 2 === ', rez22);
+
+function filterAdults2(arr) {
+    let newArr = [];
+    arr.filter(function(el) {
+        if (el.age >= 18) {
+            newArr.push({ name: el.name, hasDog: el.age });
+        }
+    });
+    return newArr;
+}
+
+const rez222 = filterDogOwers2(users);
+console.log('Pinametis 3 === ', rez222);
