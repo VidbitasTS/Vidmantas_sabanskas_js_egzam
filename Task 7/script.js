a78af15e4861a477fd930949cb0a24f3659f6e5d
vidbitas <vidbitas@gmail.com>
2022-08-18 07:08:19 +0300

    Task7 done

--- Task 7/script.js
+++ Task 7/script.js
@@ -5,8 +5,21 @@ Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
 const audi = {
-  make: 'audi',
-  model: 'A6',
-  year: 2005,
-  color: 'white',
+    make: 'audi',
+    model: 'A6',
+    year: 2005,
+    color: 'white',
 };
+
+function key(obj) {
+    let keyArr = [];
+    for (const key in obj) {
+        keyArr.push(key);
+    }
+    return keyArr;
+}
+
+const rezArr = key(audi);
+console.log(rezArr);
+
+console.log(Object.keys(audi));
