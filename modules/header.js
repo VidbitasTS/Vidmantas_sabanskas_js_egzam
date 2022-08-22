483429c460d4073bc10c0b8127ea775e6cbfbbbf
vidbitas <vidbitas@gmail.com>
2022-08-22 07:04:06 +0300

    header.js koregavimas script failu atvaizdavimas

--- modules/header.js
+++ modules/header.js
@@ -7,26 +7,11 @@ export default async function initHeader() {
     // ideti i body pabaiga
     document.querySelector('header').insertAdjacentHTML('afterbegin', htmlHeader);
 
-    // const str = document.location.toString();
-    // const path = str.slice(0, str.indexOf('index'));
-    // alert(path);
-    // createH1(path, str);
     createH1(window.location.toString().toUpperCase());
 
-    // document.querySelector('#script').addEventListener('click', () => {
-    //     openTextFile(path + 'js/script.js');
-    // });
-    // document.querySelector('#style').addEventListener('click', () => {
-    //     openTextFile(path + 'css/style.css');
-    // });
-
-    // const clickScriptEl = document.querySelectorAll('#nav-script');
-    // console.log(clickScriptEl[0].children[0].dataset.filename);
-    // clickScriptEl.addEventListener('click', (e) => {
-    //     alert(e.target);
-    // });
-
     document.querySelector('#nav-script').addEventListener('click', (e) => {
+        const str = document.location.toString();
+        const path = str.slice(0, str.indexOf('Index'));
         openTextFile(path + e.target.dataset.filename);
     });
 }
@@ -57,19 +42,6 @@ function createH1(path) {
     divh1El.className = 'divh1';
     divh1El.innerHTML = `<h1>TASK ${rez}</h1>`;
     wrapEl.appendChild(divh1El);
-
-    // console.log(nrStart, nrEnd, rez);
-
-    // return;
-    // if (path.slice(str.indexOf('%20') + 3, -1).length > 1) alert('daugiau');
-    // alert(path.slice(str.indexOf('%20') + 3, -1));
-    // if (path.slice(str.indexOf('%20') + 3, -1) < 5) return;
-    // const taskNumber = path.slice(str.indexOf('%20') + 3, -1);
-    // const wrapEl = document.querySelector('.wrapper');
-    // const divh1El = document.createElement('div');
-    // divh1El.className = 'divh1';
-    // divh1El.innerHTML = `<h1>TASK ${taskNumber}</h1>`;
-    // wrapEl.appendChild(divh1El);
 }
 
 function createModalWindow() {
